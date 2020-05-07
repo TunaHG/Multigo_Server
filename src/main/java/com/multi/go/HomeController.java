2c0b240bab26e79b88f1f7426efcf04fd019662c
TunaHG <asdf0185@naver.com>
2020-05-07 10:42:27 +0900

    Test Commit

--- src/main/java/com/multi/go/HomeController.java
+++ src/main/java/com/multi/go/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		System.out.println("Hello");
+		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
