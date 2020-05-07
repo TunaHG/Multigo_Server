728c046c67831c690a710bfae8e7df79d04060b9
TunaHG <asdf0185@naver.com>
2020-05-07 14:05:50 +0900

    Update Items Table CRUD Features (Comments)

--- src/main/java/mybatis/ItemsDAO.java
+++ src/main/java/mybatis/ItemsDAO.java
@@ -8,20 +8,36 @@ public class ItemsDAO {
 		this.session = session;
 	}	
 	
+	/*
+	 * Insert New Item Method
+	 */
 	public void addItem(ItemsVO vo) {
 		session.insert("insertItem", vo);
+		System.out.println("[Server]\t[Item]\tInsert New Item");
 	}
 	
+	/*
+	 * Get Item Data Method
+	 */
 	public ItemsVO getItem(String item_id) {
 		ItemsVO vo = session.selectOne("selectItem", item_id);
+		System.out.println("[Server]\t[Item]\tGet Item");
 		return vo;
 	}
 	
+	/*
+	 * Update Item Data Method
+	 */
 	public void updateItem(ItemsVO vo) {
 		session.update("updateItem", vo);
+		System.out.println("[Server]\t[Item]\tUpdate Item");
 	}
 	
+	/*
+	 * Delete Item Method
+	 */
 	public void deleteItem(ItemsVO vo) {
 		session.delete("deleteItem", vo);
+		System.out.println("[Server]\t[Item]\tDelete Item");
 	}
 }
